95eb5312df40e6a8239d9a4911584530437c9c3b
Josh Larson <jplhomer@gmail.com>
2018-08-06 21:21:34 -0500

    Add storage persistence

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, KeyboardAvoidingView, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView, TextInput, Button, AsyncStorage } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      thing1: '',
+      thing2: '',
+      thing3: '',
+      thing4: '',
+    }
+  }
+
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
@@ -11,18 +22,21 @@ export default class App extends React.Component {
           ref="thing1"
           placeholder="Something"
           returnKeyType="next"
+          onChangeText={thing1 => this.setState({ thing1 })}
           onSubmitEditing={() => this.refs.thing2.focus()} />
         <TextInput
           style={styles.input}
           ref="thing2"
           placeholder="Something"
           returnKeyType="next"
+          onChangeText={thing2 => this.setState({ thing2 })}
           onSubmitEditing={() => this.refs.thing3.focus()} />
         <TextInput
           style={styles.input}
           ref="thing3"
           placeholder="Something"
           returnKeyType="next"
+          onChangeText={thing3 => this.setState({ thing3 })}
           onSubmitEditing={() => this.refs.thing4.focus()} />
 
         <Text style={styles.text}>What could have gone better?</Text>
@@ -31,10 +45,11 @@ export default class App extends React.Component {
           ref="thing4"
           placeholder="Something"
           returnKeyType="done"
-          onSubmitEditing={this.onSave} />
+          onChangeText={thing4 => this.setState({ thing4 })}
+          onSubmitEditing={() => this.onSave()} />
 
         <Button
-          onPress={this.onSave}
+          onPress={() => this.onSave()}
           title="Save Update"
           accessibilityLabel="Save today's update"
         />
@@ -42,9 +57,21 @@ export default class App extends React.Component {
     );
   }
 
-  onSave() {
+  async onSave() {
     console.log('On Save fired');
+
+    const data = this.state;
+
+    try {
+      await AsyncStorage.setItem(getDateKey(), JSON.stringify(data));
+    } catch (error) {
+      // Error saving data
+    }
   }
+}
+
+function getDateKey() {
+  return '20180720';
 }
 
 const fontFamily = 'Avenir Next';
